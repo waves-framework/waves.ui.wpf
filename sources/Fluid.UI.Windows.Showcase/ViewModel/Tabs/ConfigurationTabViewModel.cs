20d0335b1ac8c7fa9000db340f4fc59f3bd2ab67
Egor Khindikaynen <khek@ambertape.ru>
2020-04-21 17:28:47 +0300

    FLUID-73 UI.Windows: Configuration tab.

---
+++ sources/Fluid.UI.Windows.Showcase/ViewModel/Tabs/ConfigurationTabViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fluid.Presentation.Base;
+
+namespace Fluid.UI.Windows.Showcase.ViewModel.Tabs
+{
+    /// <summary>
+    /// Configuration tab view model.
+    /// </summary>
+    public class ConfigurationTabViewModel : PresentationViewModel
+    {
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+        }
+    }
+}
